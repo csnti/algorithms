ed96071550e5bf072aec9dda6e609408e4e68204
csnti <63444698+csnti@users.noreply.github.com>
2022-04-24 20:20:41 -0700

    Update tribonacci-dynamic-recursive.js

--- tribonacci-dynamic-recursive.js
+++ tribonacci-dynamic-recursive.js
@@ -12,8 +12,8 @@ return cache[n] = (tribonacci(n - 1, cache) + tribonacci(n - 2, cache) + tribona
 }
 
 /**
- * base cases: if n == 0 return 0. if n <= 2 return 1
- * use memoization to speed-up expensive operations. 
- * if arguments (n) were used before, find the result in the cache object. else, store the result of tribonacci 
- * in the cache. ensure that the cache is passed on as an arguemnt within the stack. 
+ * Base cases: if n equals zero, return zero. if n is less-than or equal to two, return one.
+ * Use "memoization" to speed-up expensive operations. 
+ * If the argument n was used before, find the result in the cached object. Else, store the result of the tribonacci sequence
+ * in the cache. Ensure that the cache is passed on as an argument within every function call of the stack.
  */
