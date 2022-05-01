35a462e4e8e21e4841c7f4278f430c056936d5a1
C S <chris@MacBook-Air.local>
2022-05-01 00:05:36 -0700

    Code

---
+++ find-encrypted-words.js
@@ -0,0 +1,69 @@
+// Add any extra import statements you may need here
+
+
+// Add any helper functions you may need here
+
+function findEncryptedWord(s) {
+  if (s === "") return s;
+  let mid = s.length % 2 === 1 ? Math.floor(s.length / 2) : s.length / 2 - 1;
+  
+return s.charAt(mid) + findEncryptedWord(s.substring(0, mid)) + findEncryptedWord(s.substring(mid + 1));
+}
+// If s has an odd length, half it, the middle is rounded down.
+// If s has an even length, half it, subtract one to get the "left-most" index in the middle.
+// Get the middle char of s and append the "left side" of s (up-to mid)
+// Followed by the "right side" of s (up-to mid plus one, to start from the "right-most" char)
+// The shuffling is done recursively until the input is empty and returns an empty string, which completes the stack.
+// O(n log n) time. O(n) space.
+
+
+
+
+// These are the tests we use to determine if the solution is correct.
+// You can add your own at the bottom.
+function printString(str) {
+  var out = '["' + str + '"]';
+  return out;
+}
+
+var test_case_number = 1;
+
+function check(expected, output) {
+  var result = (expected == output);
+  var rightTick = "\u2713";
+  var wrongTick = "\u2717";
+  if (result) {
+    var out = rightTick + ' Test #' + test_case_number;
+    console.log(out);
+  }
+  else {
+    var out = '';
+    out += wrongTick + ' Test #' + test_case_number + ': Expected ';
+    out += printString(expected);
+    out += ' Your output: ';
+    out += printString(output);
+    console.log(out);
+  }
+  test_case_number++;
+}
+
+var s_1 = "abc";
+var expected_1 = "bac";
+var output_1 = findEncryptedWord(s_1);
+check(expected_1, output_1);
+
+var s_2 = "abcd";
+var expected_2 = "bacd";
+var output_2 = findEncryptedWord(s_2);
+check(expected_2, output_2);
+
+var s_2 = "abcxcba";
+var expected_2 = "xbacbca";
+var output_2 = findEncryptedWord(s_2);
+check(expected_2, output_2);
+
+var s_2 = "facebook";
+var expected_2 = "eafcobok";
+var output_2 = findEncryptedWord(s_2);
+check(expected_2, output_2);
+// Add your own test cases here
