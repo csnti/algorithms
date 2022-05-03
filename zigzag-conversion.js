7695dca84ba635f2d2b802b9069a22cea80fa64c
C S <chris@MacBook-Air.local>
2022-05-03 16:26:41 -0700

    Code

--- zigzag-conversion.js
+++ zigzag-conversion.js
@@ -25,3 +25,8 @@ function convert(s, numRows) {
   }
   return rows.join('');
 };
+
+// let rows = Array(numRows).fill(''); creates an array of empty strings like this : ["", "", ""] according to the number of rows.
+// The "currentRow" decides which of these strings gets s[i] appended to it.
+// The if-statements determine the direction of the zigzag pattern, it can only go up or down.
+// Array.join('') flattens all the strings into one, providing the result.
