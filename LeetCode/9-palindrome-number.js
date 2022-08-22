bd8f5c60b670a11342e666c2eaeb6a7e1ecfd346
Dev <dev@MacBook-Air.local>
2022-08-22 15:33:01 -0700

    Code

--- LeetCode/9-palindrome-number.js
+++ LeetCode/9-palindrome-number.js
@@ -5,8 +5,7 @@ function isPalindrome (x) {
   let p1 = 0;
   let p2 = str.length-1;
 
-  while (p1 < p2) { 
-    console.log(str[p1], str[p2])
+  while (p1 < p2) {
     if (str[p1] === str[p2]) {
       p1++;
       p2--;
@@ -15,6 +14,6 @@ function isPalindrome (x) {
       return false;
     }    
   }
-    
+
   return true;
 }
