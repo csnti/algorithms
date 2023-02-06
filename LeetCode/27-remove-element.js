545a28bc3debebe46a392e91e0bba919148f20a6
Dev <github@chrissantiago.com>
2023-02-06 13:18:59 -0800

    Code

--- LeetCode/27-remove-element.js
+++ LeetCode/27-remove-element.js
@@ -1,6 +1,16 @@
 https://leetcode.com/problems/remove-element/
 
 function removeElement(nums, val) {
-
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
   
 }
+
+removeElement([3,2,2,3], 3)
+removeElement([0,1,2,2,3,0,4,2], 2)
